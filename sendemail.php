0722db84a15269862c83715453c65a07fef98f64
danish <danish9412@gmail.com>
2015-02-15 16:39:04 +0530

    google analytics added

--- sendemail.php
+++ sendemail.php
@@ -1,22 +1,14 @@
-<?php
-	header('Content-type: application/json');
-	$status = array(
-		'type'=>'success',
-		'message'=>'Email sent!'
-	);
-
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $number = @trim(stripslashes($_POST['number'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
-
-    $email_from = $email;
-    $email_to = 'fostertraining.delhi@gmail.com, foster@fostertraining.in';
-
-    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
-
-    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
-
-    echo json_encode($status);
-    die;
+<?php 
+$ToEmail = 'jcnasa@gmail.com'; 
+$EmailSubject = $_POST["subjectofperson"]; 
+$mailheader = "From: ".$_POST["emailofperson"]. ; 
+$mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+$MESSAGE_BODY = "Name: ".$_POST["nameofperson"]."<br>"; 
+$MESSAGE_BODY .= "Email: ".$_POST["emailofperson"]."<br>"; 
+$MESSAGE_BODY .= "Organisation: ".$_POST["orgofperson"]."<br>";
+$MESSAGE_BODY .= "Location: ".$_POST["locofperson"]."<br>";
+$MESSAGE_BODY .= "Message: ".nl2br($_POST["messageofperson"])."<br>"; 
+mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure! Try again."); 
+echo "Message recorded. We will get back to you soon" . "-" . "<a href='www.fostertraining.in' style='text-decoration:none;color:#ff0099;'>Click here to return</a>";
+?>
